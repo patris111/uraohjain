d4af08b6c55bf1e1cf045e191dfb5a01243df35c
nedpat <74729837+nedpat@users.noreply.github.com>
2023-08-30 11:12:34 +0300

    Update form with floating labels & add image

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -2,85 +2,99 @@ import React from 'react';
 
 const SignupForm = () => {
   return (
-    <div>
-      <form class='w-full max-w-lg'>
-        <h3 class='mb-4'>Henkilötiedot</h3>
-        <div class='flex flex-wrap mb-8'>
-          <div class='w-full md:w-1/2 pr-6 mb-8 md:mb-0'>
+    <div className='grid grid-cols-2 gap-4'>
+      <form className=''>
+        <h3 className='mt-0 mb-12'>Henkilötiedot</h3>
+
+        <div className='flex flex-wrap mb-8'>
+          <div className='md:w-1/2 pr-6 relative z-0 w-full mb-6 group'>
+            <input
+              className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+              id='first-name'
+              type='text'
+              required
+            />
             <label
-              class='block  tracking-wide burgundy-color text-lg font-semibold mb-2'
+              className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='first-name'
             >
               Etunimi
             </label>
-            <input
-              class='appearance-none block w-full bg-white burgundy-color border rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500'
-              id='first-name'
-              type='text'
-            />
           </div>
-          <div class='w-full md:w-1/2'>
+          <div className='md:w-1/2 relative z-0 w-full mb-6 group'>
             <label
-              class='block  tracking-wide burgundy-color text-lg font-semibold mb-2'
+              className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='last-name'
             >
               Sukunimi
             </label>
             <input
-              class='appearance-none block w-full bg-white burgundy-color border border-gray-200 rounded py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500'
+              className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
               id='last-name'
               type='text'
             />
           </div>
         </div>
-        <div class='flex flex-wrap mb-8'>
-          <div class='w-full md:w-2/3 pr-6'>
+        <div className='flex flex-wrap mb-8'>
+          <div className='md:w-2/3 pr-6 relative z-0 w-full mb-6 group'>
             <label
-              class='block  tracking-wide burgundy-color text-lg font-semibold mb-2'
+              className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='email'
             >
               Sähköposti
             </label>
             <input
-              class='appearance-none block w-full bg-white burgundy-color border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500'
+              className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
               id='email'
               type='text'
             />
           </div>
-          <div class='w-full md:w-1/3'>
-            <label class='block tracking-wide burgundy-color text-lg font-semibold mb-2' for='age'>
+          <div className='md:w-1/3 relative z-0 w-full mb-6 group'>
+            <label
+              className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              for='age'
+            >
               Ikä
             </label>
             <input
-              class='appearance-none block w-full bg-white burgundy-color border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500'
+              className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
               id='age'
               type='text'
             />
           </div>
         </div>
-        <div class='w-full mb-8'>
+        <div className='relative z-0 w-full mb-6 group'>
           <label
-            class='block  tracking-wide burgundy-color text-lg font-semibold mb-2'
+            className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
             for='description'
           >
             Kerro lyhyesti työkokemuksestasi; Millaisia työnimikkeitä ja tehtäviä sinulla on ollut?
           </label>
           <textarea
-            class='appearance-none block w-full bg-white burgundy-color border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500'
+            className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
             id='description'
+            rows='1'
           />
         </div>
-        <div class='w-full mb-8'>
-          <label class='block  tracking-wide burgundy-color text-lg font-semibold mb-2' for='cv'>
+        <div className='relative z-0 w-full mb-6 group'>
+          <label
+            className='peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+            for='cv'
+          >
             CV, Linkedin (vapaaehtoinen)
           </label>
           <input
-            class='appearance-none block w-full bg-white burgundy-color border border-gray-200 rounded py-3 px-4 mb-3 focus:outline-none focus:bg-white focus:border-gray-500'
+            className='block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
             id='cv'
             type='text'
           />
         </div>
       </form>
+      <img
+        src='https://plus.unsplash.com/premium_photo-1668017178993-4c8fc9f59872?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60'
+        alt=''
+        className='w-full h-full'
+      />
     </div>
   );
 };
