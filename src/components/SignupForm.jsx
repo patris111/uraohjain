e0b27c40c0575e97ed678a135ab90e75e372f194
nedpat <74729837+nedpat@users.noreply.github.com>
2023-08-31 14:26:13 +0300

    Improve signup form style and topography

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -4,98 +4,98 @@ const SignupForm = () => {
   return (
     <div className='grid grid-cols-2 gap-4'>
       <form className='p-14'>
-        <h3 className='mt-0 mb-24'>Henkilötiedot</h3>
+        <h3 className='mt-0 mb-20'>Henkilötiedot</h3>
 
         <div className='flex flex-wrap mb-8'>
           <div className='md:w-1/2 pr-6 relative z-0 w-full mb-6 group'>
-            <input
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='first-name'
-              type='text'
-              required
-            />
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='first-name'
             >
               Etunimi
             </label>
-          </div>
-          <div className='md:w-1/2 relative z-0 w-full mb-6 group'>
             <input
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='last-name'
+              className='block py-2.5 px-0 w-full text-lg font-semibold text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='first-name'
               type='text'
               required
             />
+          </div>
+          <div className='md:w-1/2 relative z-0 w-full mb-6 group'>
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='last-name'
             >
               Sukunimi
             </label>
+            <input
+              className='block py-2.5 px-0 w-full text-lg font-semibold text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='last-name'
+              type='text'
+              required
+            />
           </div>
         </div>
         <div className='flex flex-wrap mb-8'>
           <div className='md:w-2/3 pr-6 relative z-0 w-full mb-6 group'>
-            <input
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='email'
-              type='text'
-              required
-            />
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='email'
             >
               Sähköposti
             </label>
-          </div>
-          <div className='md:w-1/3 relative z-0 w-full mb-6 group'>
             <input
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='age'
+              className='block py-2.5 px-0 w-full text-lg font-semibold text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='email'
               type='text'
               required
             />
+          </div>
+          <div className='md:w-1/3 relative z-0 w-full mb-6 group'>
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='age'
             >
               Ikä
             </label>
+            <input
+              className='block py-2.5 px-0 w-full text-lg font-semibold text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='age'
+              type='text'
+              required
+            />
           </div>
         </div>
         <div className='flex flex-wrap mb-8'>
           <div className='relative z-0 w-full mb-6 group'>
-            <textarea
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='description'
-              rows='1'
-              required
-            />
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='description'
             >
               Kerro lyhyesti työkokemuksestasi; Millaisia työnimikkeitä ja tehtäviä sinulla on
               ollut?
             </label>
+            <textarea
+              className='mt-6 block p-4 w-full text-lg font-semibold text-gray-950 bg-transparent border-2  border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='description'
+              rows='4'
+              required
+            />
           </div>
         </div>
         <div className='flex flex-wrap mb-8'>
           <div className='relative z-0 w-full mb-6 group'>
-            <input
-              className='block py-2.5 px-0 w-full text-xl text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
-              id='cv'
-              type='text'
-            />
             <label
-              className='font-bold peer-focus:font-bold absolute text-2xl text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+              className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='cv'
             >
               CV, Linkedin (vapaaehtoinen)
             </label>
+            <input
+              className='block py-2.5 px-0 w-full text-lg font-semibold text-gray-950 bg-transparent border-0 border-b-2 border-red-900 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer'
+              id='cv'
+              type='text'
+            />
           </div>
         </div>
         <button class='btn btn-outline float-right'>Lähetä</button>
