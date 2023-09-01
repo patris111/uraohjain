e5e0fa909002eafe1c1845bfb177b90ba86f303b
nedpat <74729837+nedpat@users.noreply.github.com>
2023-09-01 19:22:58 +0300

    Add title, text & smaller video in video section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image(), react()],
 });
