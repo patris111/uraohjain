f7b3a7860b16b685d5a6265ed175d8d0cc91cc56
nedpat <74729837+nedpat@users.noreply.github.com>
2023-09-07 15:15:47 +0300

    Implement NewsCardAPI & use it in news-api page

--- src/Interfaces/article.ts
+++ src/Interfaces/article.ts
@@ -9,6 +9,9 @@ export default interface Article {
             thumbnail: {
               url: string;
             };
+            small: {
+              url: string;
+            };
           };
         };
       };
--- src/js/utils.js
+++ src/js/utils.js
@@ -5,3 +5,15 @@ export function formatDate(date) {
     year: 'numeric',
   });
 }
+
+export function truncateText(text, maxCharacters = 100, readMoreText = 'Read more') {
+  // Check if the text length is greater than the maximum allowed characters
+  if (text.length > maxCharacters) {
+    // Truncate the text to the specified number of characters
+    const truncatedText = text.slice(0, maxCharacters);
+    return `${truncatedText} ${readMoreText}`;
+  } else {
+    // If not truncated, return the original text
+    return text;
+  }
+}
