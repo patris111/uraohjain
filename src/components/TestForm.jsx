ece7d60ae574b3596cf1e2a3fdbd711c903f8763
nedpat <74729837+nedpat@users.noreply.github.com>
2023-09-21 15:29:54 +0300

    Position next button in mobile view

--- src/components/TestForm.jsx
+++ src/components/TestForm.jsx
@@ -133,9 +133,9 @@ const TestForm = () => {
             />
             <span className='mx-4 font-bold'>Ei</span>
           </ol>
-          <div className='flex justify-end mt-4'>
+          <div className='flex justify-end mt-10 md:mt-4'>
             <button
-              className={`btn btn-lg btn-outline float-right w-40 ${
+              className={`btn btn-lg btn-outline w-40 ${
                 allRadioChecked ? '' : 'cursor-not-allowed opacity-80'
               }`}
               onClick={handleNextClick}
