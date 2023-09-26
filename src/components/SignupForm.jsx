20f8dade249f0725d3125a430130c73d746de2c5
nedpat <74729837+nedpat@users.noreply.github.com>
2023-09-26 08:46:18 +0300

    Set 100% opacity to partners logos

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const SignupForm = () => {
   return (
-    <div className='grid grid-cols-2 gap-4'>
-      <form className='p-14'>
+    <div className='md:grid md:grid-cols-2 md:gap-4'>
+      <form className='p-8 md:p-14'>
         <h3 className='mt-0 mb-20'>Henkilötiedot</h3>
 
         <div className='flex flex-wrap mb-8'>
-          <div className='md:w-1/2 pr-6 relative z-0 w-full mb-6 group'>
+          <div className='md:w-1/2 md:pr-6 relative z-0 w-full mb-6 group'>
             <label
               className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='first-name'
@@ -37,7 +37,7 @@ const SignupForm = () => {
           </div>
         </div>
         <div className='flex flex-wrap mb-8'>
-          <div className='md:w-2/3 pr-6 relative z-0 w-full mb-6 group'>
+          <div className='md:w-2/3 md:pr-6 relative z-0 w-full mb-6 group'>
             <label
               className='font-bold peer-focus:font-bold text-lg text-red-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
               for='email'
@@ -98,12 +98,14 @@ const SignupForm = () => {
             />
           </div>
         </div>
-        <button class='btn btn-outline float-right'>Lähetä</button>
+        <div class='flex justify-end'>
+          <button class='btn btn-outline'>Lähetä</button>
+        </div>
       </form>
       <img
         src='https://images.unsplash.com/photo-1519070543065-ac1764985512?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=6095&q=80'
         alt=''
-        className='w-full h-full'
+        className='hidden md:block md:w-full md:h-full'
       />
     </div>
   );
