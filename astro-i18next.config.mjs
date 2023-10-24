10efd2c0dce70b51e4b82a8befcb9cec0bef386a
nedpat <74729837+nedpat@users.noreply.github.com>
2023-10-24 13:32:57 +0300

    Add basic setup for astro-i18next

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: 'fi',
+  locales: ['fi', 'en'],
+  namespaces: ['home'],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from 'astro-i18next';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
@@ -6,5 +7,5 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()],
+  integrations: [tailwind(), image(), react(), astroI18next()],
 });
